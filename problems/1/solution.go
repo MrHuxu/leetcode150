158f472fa8d991373763152a6726ed1e0b81bf05
xhu <xhuxu@didiglobal.com>
2019-09-29 21:48:40 +0800

    update the solution and explaination of problem 1

--- problems/1/solution.go
+++ problems/1/solution.go
@@ -1,7 +1,7 @@
 package leetcode150
 
 func twoSum(nums []int, target int) []int {
-	mapNumToPos := make(map[int]int)
+	mapNumToPos := make(map[int]int, len(nums))
 	for i, v := range nums {
 		if pos, ok := mapNumToPos[target-v]; ok {
 			return []int{pos, i}
