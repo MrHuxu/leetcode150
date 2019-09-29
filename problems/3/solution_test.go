a0cb3165b621ecbc69078c623a72072299243173
xhu <xhuxu@didiglobal.com>
2019-09-30 01:17:37 +0800

    add the solution of problem 3

---
+++ problems/3/solution_test.go
@@ -0,0 +1,14 @@
+package leetcode150
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(3, lengthOfLongestSubstring("abcabcbb"))
+	assert.Equal(1, lengthOfLongestSubstring("bbbbb"))
+}
