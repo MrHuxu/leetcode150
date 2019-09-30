960aa653cece12b44f9a93b9573bebc98494c49e
MrHuxu <hxtheone@gmail.com>
2019-09-30 15:20:48 +0800

    finish the index page style

--- website/client/components/detail.jsx
+++ website/client/components/detail.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { string } from 'prop-types';
 import marked from 'marked';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #eee;
+`;
 
 const Detail = ({ solution, explanation }) => {
   return (
-    <div>
+    <Container>
       <pre><code lang="golang">{ solution }</code></pre>
       <div dangerouslySetInnerHTML={ { __html: marked(explanation) } } />
-    </div>
+    </Container>
   );
 };
 
--- website/client/components/index.jsx
+++ website/client/components/index.jsx
@@ -1,22 +1,82 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import data from '../data';
 
+const Container = styled.div`
+  background-color: #eee;
+`;
+const Left = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 30%;
+  text-align: center;
+  padding: 24px 0 0 0;
+  color: #555;
+`;
+const Right = styled.div`
+  padding: 24px 0 30px 30%;
+`;
+const LinkContainer = styled.div`
+  margin: 12px 0;
+`;
+const Link = styled.a`
+  color: #666;
+  font-size: 15px;
+`;
+const difficultyContainers = {
+  1 : styled.span`
+    background-color: #5cb85c;
+  `,
+  2 : styled.span`
+    background-color: #f0ad4e;
+  `,
+  3 : styled.span`
+    background-color: #d9534f;
+  `
+};
+const difficultyLabels = {
+  1 : 'Easy',
+  2 : 'Medium',
+  3 : 'Hard'
+};
+
 const Index = () => {
   return (
-    <div>
-      <h1>LeetCode 150</h1>
-      { data.map(item => (
-        <div>
-          <a
-            key={ item.id }
-            href={ `/${item.id}` }
-          >
-            { `${item.id} ${item.title}` }
-          </a>
-        </div>
-      )) }
-    </div>
+    <Container>
+      <Left>
+        <h1>LeetCode 150</h1>
+      </Left>
+
+      <Right>
+        { data.map(item => {
+          const DifficultyContainer = difficultyContainers[item.difficulty];
+          const difficultyLabel = difficultyLabels[item.difficulty];
+
+          return (
+            <LinkContainer>
+              <Link
+                key={ item.id }
+                href={ `/${item.id}` }
+              >
+                { `${item.id}. ${item.title}` }
+              </Link>
+              <DifficultyContainer
+                style={ {
+                  color        : '#fff',
+                  fontSize     : 11,
+                  padding      : '0 2px',
+                  borderRadius : 3,
+                  marginLeft   : 8
+                } }
+              >
+                { difficultyLabel }
+              </DifficultyContainer>
+            </LinkContainer>
+          );
+        }) }
+      </Right>
+    </Container>
   );
 };
 
