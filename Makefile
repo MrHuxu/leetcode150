bac292207f972fee8d44c86d24f365198f4d9fb0
xhu <xhuxu@didiglobal.com>
2019-10-06 10:32:23 +0800

    update makefile and the code of 8

--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ test:
 solve:
 	mkdir problems/$(id)
 	echo 'package leetcode150' > problems/$(id)/solution.go
-	echo 'package leetcode150' > problems/$(id)/solution_test.go
+	echo 'package leetcode150\n\nimport (\n  "testing"\n\n  "github.com/stretchr/testify/assert"\n)\n\nfunc Test_func(t *testing.T) {\n  assert := assert.New(t)\n}' > problems/$(id)/solution_test.go
 	echo '# 题意\n\n\n# 解答' > problems/$(id)/explanation.md
---
+++ problems/8/solution.go
@@ -0,0 +1,40 @@
+package leetcode150
+
+func myAtoi(str string) int {
+	num := 0
+	sym := 1
+	started := false
+	for i := 0; i < len(str); i++ {
+		b := str[i]
+
+		if started {
+			if b >= 48 && b <= 57 {
+				num = num*10 + (int(b)-48)*sym
+
+				if num > 2147483647 {
+					return 2147483647
+				} else if num < -2147483648 {
+					return -2147483648
+				}
+			} else {
+				break
+			}
+		} else {
+			if b == 32 {
+				continue
+			} else if b == 43 {
+				started = true
+			} else if b == 45 {
+				started = true
+				sym = -1
+			} else if b >= 48 && b <= 57 {
+				started = true
+				num = sym * (int(b) - 48)
+			} else {
+				break
+			}
+		}
+	}
+
+	return num
+}
