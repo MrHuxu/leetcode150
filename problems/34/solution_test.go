9a021794152f78512fb5db194efa3c611344d364
qhan <qhan@freewheel.tv>
2019-10-11 08:42:22 +0800

    update go.sum and add the code of problem 34

---
+++ problems/34/solution_test.go
@@ -0,0 +1,18 @@
+package leetcode150
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_searchRange(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(
+		[]int{3, 4}, searchRange([]int{5, 7, 7, 8, 8, 10}, 8),
+	)
+	assert.Equal(
+		[]int{-1, -1}, searchRange([]int{5, 7, 7, 8, 8, 10}, 6),
+	)
+}
