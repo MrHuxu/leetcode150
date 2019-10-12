9f4198a0a1bee4ec74d8ec4dd2fd83ef7b01e8cf
MrHuxu <hxtheone@gmail.com>
2019-10-13 01:12:09 +0800

    add the code and test cases of problem 46

---
+++ problems/46/solution_test.go
@@ -0,0 +1,20 @@
+package leetcode150
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_func(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal([][]int{
+		[]int{1, 2, 3},
+		[]int{1, 3, 2},
+		[]int{2, 1, 3},
+		[]int{2, 3, 1},
+		[]int{3, 1, 2},
+		[]int{3, 2, 1},
+	}, permute([]int{1, 2, 3}))
+}
