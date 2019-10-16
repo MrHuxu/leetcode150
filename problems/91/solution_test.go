299718e8142d27e87d1190cbc3284dbfb57bddb0
MrHuxu <hxtheone@gmail.com>
2019-10-17 01:07:48 +0800

    add the code and test cases of problem 91

---
+++ problems/91/solution_test.go
@@ -0,0 +1,17 @@
+package leetcode150
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_func(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(2, numDecodings("12"))
+	assert.Equal(3, numDecodings("226"))
+	assert.Equal(0, numDecodings("0"))
+	assert.Equal(0, numDecodings("01"))
+	assert.Equal(1, numDecodings("10"))
+}
