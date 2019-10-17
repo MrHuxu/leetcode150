1966bd00ab6b33c40cb072a6d5aa0d877d7a6f24
MrHuxu <hxtheone@gmail.com>
2019-10-17 21:33:29 +0800

    add code and test cases of problem 98 100 101 102 103 104

---
+++ problems/103/solution.go
@@ -0,0 +1,48 @@
+package leetcode150
+
+import . "github.com/MrHuxu/leetcode150/problems/utils"
+
+// code
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	var result [][]int
+	var fromRight bool
+
+	level := []*TreeNode{root}
+	for len(level) != 0 {
+		var vals []int
+		var nextLevel []*TreeNode
+
+		for i := 0; i < len(level); i++ {
+			if fromRight {
+				vals = append(vals, level[len(level)-1-i].Val)
+			} else {
+				vals = append(vals, level[i].Val)
+			}
+
+			if level[i].Left != nil {
+				nextLevel = append(nextLevel, level[i].Left)
+			}
+			if level[i].Right != nil {
+				nextLevel = append(nextLevel, level[i].Right)
+			}
+		}
+
+		fromRight = !fromRight
+		result = append(result, vals)
+		level = nextLevel
+	}
+
+	return result
+}
---
+++ problems/98/solution.go
@@ -0,0 +1,56 @@
+package leetcode150
+
+import . "github.com/MrHuxu/leetcode150/problems/utils"
+
+// code
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	_, _, valid := validate(root)
+	return valid
+}
+
+func validate(root *TreeNode) (int, int, bool) {
+	max := root.Val
+	min := root.Val
+
+	if root.Left != nil {
+		leftMax, leftMin, leftValid := validate(root.Left)
+		if leftValid {
+			if leftMax >= root.Val {
+				println(1)
+				return 0, 0, false
+			}
+			min = leftMin
+		} else {
+			println(2)
+			return 0, 0, false
+		}
+	}
+
+	if root.Right != nil {
+		rightMax, rightMin, rightValid := validate(root.Right)
+		if rightValid {
+			if rightMin <= root.Val {
+				println(3)
+				return 0, 0, false
+			}
+			max = rightMax
+		} else {
+			println(4)
+			return 0, 0, false
+		}
+	}
+
+	return max, min, true
+}
