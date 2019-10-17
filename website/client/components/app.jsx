344579507769683d6147ed473c7357cadb658979
MrHuxu <hxtheone@gmail.com>
2019-10-17 15:16:26 +0800

    fix proptypes error

--- website/client/components/app.jsx
+++ website/client/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { node } from 'prop-types';
 
 const Container = styled.div`
   max-width: 800px;
@@ -18,6 +19,10 @@ const App = ({ children }) => {
       { children }
     </Container>
   );
+};
+
+App.propTypes = {
+  children : node.isRequired
 };
 
 export default App;
