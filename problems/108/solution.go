cbdab261e0da4b771231363e97d71f8e9a84d5c0
MrHuxu <hxtheone@gmail.com>
2019-10-18 00:42:34 +0800

    add code and test cases of problem 107 108 109

---
+++ problems/108/solution.go
@@ -0,0 +1,26 @@
+package leetcode150
+
+import . "github.com/MrHuxu/leetcode150/problems/utils"
+
+// code
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := (0 + len(nums) - 1) / 2
+
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBST(nums[0:mid])
+	root.Right = sortedArrayToBST(nums[mid+1 : len(nums)])
+
+	return root
+}
---
+++ problems/109/solution_test.go
@@ -0,0 +1,18 @@
+package leetcode150
+
+import (
+	"testing"
+
+	. "github.com/MrHuxu/leetcode150/problems/utils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_sortedListToBST(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(
+		BuildTree([]interface{}{0, -10, 5, nil, -3, nil, 9}),
+		sortedListToBST(BuildList([]int{-10, -3, 0, 5, 9})),
+	)
+}
