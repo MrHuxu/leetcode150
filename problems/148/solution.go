e88c8521203eaf3f1ea967835ded5cb293b62aa2
MrHuxu <hxtheone@gmail.com>
2019-10-20 01:40:30 +0800

    add the tree utils code and the code and test cases of 146 147 148

---
+++ problems/148/solution.go
@@ -0,0 +1,62 @@
+package leetcode150
+
+import . "github.com/MrHuxu/leetcode150/problems/utils"
+
+// code
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func sortList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	smaller := &ListNode{}
+	smallerPos := smaller
+	greater := &ListNode{}
+	greaterPos := greater
+	equal := &ListNode{Val: head.Val}
+	equalPos := equal
+
+	node := head
+	for node != nil {
+		switch {
+		case node.Val < equal.Val:
+			smallerPos.Next = node
+			smallerPos = smallerPos.Next
+
+		case node.Val > equal.Val:
+			greaterPos.Next = node
+			greaterPos = greaterPos.Next
+
+		default:
+			equalPos.Next = node
+			equalPos = equalPos.Next
+		}
+
+		node = node.Next
+	}
+	smallerPos.Next = nil
+	greaterPos.Next = nil
+	equalPos.Next = nil
+
+	smaller = sortList(smaller.Next)
+	greater = sortList(greater.Next)
+
+	newHead := &ListNode{Next: smaller}
+	tmp := newHead
+	for tmp.Next != nil {
+		tmp = tmp.Next
+	}
+	tmp.Next = equal.Next
+	for tmp.Next != nil {
+		tmp = tmp.Next
+	}
+	tmp.Next = greater
+
+	return newHead.Next
+}
