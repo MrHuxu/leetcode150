45942d6da4f0f66ea2cbcf7412832fcfb1ec4d44
MrHuxu <hxtheone@gmail.com>
2019-12-02 15:42:17 +0800

    make the content at the left of the page

--- website/client/components/app.jsx
+++ website/client/components/app.jsx
@@ -5,7 +5,6 @@ import { node } from 'prop-types';
 const Container = styled.div`
   max-width: 800px;
   padding: 30px 10px;
-  margin-left: calc(50% - 400px);
 
   @media (max-width: 800px) {
     padding: 10px 10px;
