e0587d9d1c833bdff89ea4abaed8956a26001df7
qhan <qhan@freewheel.tv>
2019-12-25 10:24:42 +0800

    add the solution of 31 32 33

--- problems/32_longest_valid_parentheses/main.go
+++ problems/32_longest_valid_parentheses/main.go
@@ -4,14 +4,14 @@ package main
 func longestValidParentheses(s string) int {
 	var result int
 
-	dp := make([]int, len(s)+1)
+	dp := make([]int, len(s))
 	for i := 1; i < len(s); i++ {
 		if s[i] == ')' {
 			if i-1-dp[i-1] >= 0 && s[i-1-dp[i-1]] == '(' {
 				dp[i] = dp[i-1] + 2
 
-				if i-2-dp[i-1] >= 0 && dp[i-2-dp[i-1]] > 0 {
-					dp[i] += dp[i-2-dp[i-1]]
+				if i-dp[i] >= 0 && dp[i-dp[i]] > 0 {
+					dp[i] += dp[i-dp[i]]
 				}
 
 				result = max(result, dp[i])
