efe9f581e2270d00947afc9dae60fecc59d1cf30
MrHuxu <hxtheone@gmail.com>
2022-05-06 11:29:47 +0800

    init java solution directory

---
+++ java/src/main/java/me/xhu/leetcode150/question_1/Solution.java
@@ -0,0 +1,22 @@
+package me.xhu.leetcode150.question_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+        int ret[] = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (m.containsKey(target - nums[i])) {
+                ret[0] = m.get(target - nums[i]);
+                ret[1] = i;
+                break;
+            }
+            m.put(nums[i], i);
+        }
+
+        return ret;
+    }
+}
