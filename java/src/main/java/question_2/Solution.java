721871870e1f0bc68273096235190ee14831f0aa
MrHuxu <hxtheone@gmail.com>
2022-05-06 18:14:54 +0800

    update the question2 solution of java

--- java/src/main/java/question_2/Solution.java
+++ java/src/main/java/question_2/Solution.java
@@ -14,31 +14,25 @@ import util.ListNode;
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode();
+        return addTwoNumbers(l1, l2, 0);
+    }
 
-        ListNode tmp = dummyHead;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            if (l1 != null && l2 != null) {
-                carry += l1.val + l2.val;
-                l1 = l1.next;
-                l2 = l2.next;
-            } else if (l1 != null) {
-                carry += l1.val;
-                l1 = l1.next;
-            } else if (l2 != null) {
-                carry += l2.val;
-                l2 = l2.next;
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 != null && l2 != null) {
+            carry += l1.val + l2.val;
+            return new ListNode(carry % 10, addTwoNumbers(l1.next, l2.next, carry / 10));
+        } else if (l1 != null) {
+            carry += l1.val;
+            return new ListNode(carry % 10, addTwoNumbers(l1.next, l2, carry / 10));
+        } else if (l2 != null) {
+            carry += l2.val;
+            return new ListNode(carry % 10, addTwoNumbers(l1, l2.next, carry / 10));
+        } else {
+            if (carry == 0) {
+                return null;
+            } else {
+                return new ListNode(carry);
             }
-
-            tmp.next = new ListNode(carry % 10);
-            tmp = tmp.next;
-            carry /= 10;
         }
-        if (carry != 0) {
-            tmp.next = new ListNode(carry);
-        }
-
-        return dummyHead.next;
     }
 }
