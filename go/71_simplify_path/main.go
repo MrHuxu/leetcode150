38ef4b00da80ddae480f88f18b13be801cd24491
xhu <hxtheone@gmail.com>
2023-04-12 11:32:14 +0800

    update go solution of 71

--- go/71_simplify_path/main.go
+++ go/71_simplify_path/main.go
@@ -5,82 +5,17 @@ import "strings"
 // code
 func simplifyPath(path string) string {
 	var stack []string
-
-	for i := 0; i < len(path); i++ {
-		if ele := path[i]; ele == '/' {
-			if len(stack) == 0 {
-				stack = append(stack, "/")
-			} else {
-				switch stack[len(stack)-1] {
-				case "/":
-					continue
-
-				case ".":
-					stack = stack[0 : len(stack)-1]
-
-				case "..":
-					var countSlash int
-					for len(stack) > 0 {
-						if stack[len(stack)-1] == "/" {
-							if countSlash == 0 {
-								countSlash++
-								stack = stack[0 : len(stack)-1]
-							} else {
-								break
-							}
-						} else {
-							stack = stack[0 : len(stack)-1]
-						}
-					}
-					if len(stack) == 0 {
-						stack = []string{"/"}
-					}
-
-				default:
-					stack = append(stack, "/")
-				}
-			}
-		} else {
-			if len(stack) == 0 {
-				stack = []string{string([]byte{ele})}
-			} else {
-				if stack[len(stack)-1] == "/" {
-					stack = append(stack, string([]byte{ele}))
-				} else {
-					stack[len(stack)-1] += string([]byte{ele})
-				}
-			}
+	for _, dir := range strings.Split(path, "/") {
+		if dir == "" || dir == "." {
+			continue
 		}
-	}
-
-	if len(stack) > 1 {
-		switch stack[len(stack)-1] {
-		case "/":
-			stack = stack[0 : len(stack)-1]
-
-		case ".":
-			stack = stack[0 : len(stack)-2]
-
-		case "..":
-			var countSlash int
-			for len(stack) > 0 {
-				if stack[len(stack)-1] == "/" {
-					stack = stack[0 : len(stack)-1]
-
-					if countSlash == 0 {
-						countSlash++
-					} else {
-						break
-					}
-				} else {
-					stack = stack[0 : len(stack)-1]
-				}
+		if dir == ".." {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
 			}
+			continue
 		}
+		stack = append(stack, dir)
 	}
-
-	if len(stack) == 0 {
-		stack = []string{"/"}
-	}
-	return strings.Join(stack, "")
+	return "/" + strings.Join(stack, "/")
 }
