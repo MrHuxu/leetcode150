cee5e273c1c3f1e269c4561374e70a2b68454890
xhu <hxtheone@gmail.com>
2023-04-26 15:51:10 +0800

    update the solution of python and code extractor

--- python/104.py
+++ python/104.py
@@ -13,9 +13,7 @@ from tree import TreeNode, buildTree
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 0 if root is None else 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
 class TestSolution(unittest.TestCase):
