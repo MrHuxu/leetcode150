ef12a4393fe0b22a6b523a0f4c65d9bfecbd2a81
xhu <hxtheone@gmail.com>
2023-06-30 15:47:03 +0800

    update the test of 49

--- go/49_group_anagrams/main_test.go
+++ go/49_group_anagrams/main_test.go
@@ -10,8 +10,8 @@ func Test_groupAnagrams(t *testing.T) {
 	assert := assert.New(t)
 
 	assert.Equal([][]string{
-		{"bat"},
 		{"eat", "tea", "ate"},
 		{"tan", "nat"},
+		{"bat"},
 	}, groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
 }
