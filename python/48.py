36f06b4d6ce8dc528dc2974311bfbf67804f06a9
xhu <hxtheone@gmail.com>
2023-09-05 12:05:12 +0800

    update python solution of 48

--- python/48.py
+++ python/48.py
@@ -4,9 +4,6 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         n = len(matrix)
         if n <= 1:
             return
