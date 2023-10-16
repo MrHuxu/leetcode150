5e2beb5270fd73588359e5680d7dc8045656b54b
xhu <hxtheone@gmail.com>
2023-10-16 11:29:56 +0800

    add ppython solution of 119

---
+++ python/119.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(1, rowIndex + 1):
+            next_row = [0] * (i + 1)
+            for j in range(i + 1):
+                val = 0
+                if j > 0:
+                    val = row[j - 1]
+                if j < i:
+                    val += row[j]
+                next_row[j] = val
+            row = next_row
+        return row
